30f5a713689152dff961b1e64ec4b299ce5e3769
Vivekraj K R <vivekraj.kr@smartr365.com>
2023-06-21 16:06:45 +0530

    Update routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,19 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { NgModule } from '@angular/core';
 import { UsersComponent } from './components/users/users.component';
+import { LifecycleHooksComponent } from './components/lifecycle-hooks/lifecycle-hooks.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 
 const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'lifecycle-hooks',
+    pathMatch:'full'
+  },
+  {
+    path: 'lifecycle-hooks',
+    component: LifecycleHooksComponent,
+  },
   {
     path: 'github-users',
     component: UsersComponent,
